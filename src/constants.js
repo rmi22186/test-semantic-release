41e209957a5cbb065ac43bfd91b8c89285c4f45a
Robert Ing <ring@mparticle.com>
2021-04-26 14:30:11 -0400

    fix: Add key4

--- src/constants.js
+++ src/constants.js
@@ -7,6 +7,7 @@ var Constants = {
     key1: 'value1',
     key2: 'value2',
     key3: 'value3',
+    key4: 'value4',
     Messages: {
         ErrorMessages: {
             NoToken: 'A token must be specified.',
